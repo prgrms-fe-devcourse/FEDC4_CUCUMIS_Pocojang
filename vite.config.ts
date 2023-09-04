b446aad8464dcc5da26eab1d295fd91e27060430
kutta97 <yhjin04170@gmail.com>
2023-09-04 15:49:02 +0900

    chore: path alias setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from 'path';
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@/public',
+        replacement: path.resolve(__dirname, ''),
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
 })

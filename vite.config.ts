aea4d61a39f7cdf4d6462faf99798259c1a96c87
kutta97 <yhjin04170@gmail.com>
2023-09-04 16:10:46 +0900

    chore: lint 및 prettier 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,4 +18,4 @@ export default defineConfig({
       },
     ],
   },
-})
+});

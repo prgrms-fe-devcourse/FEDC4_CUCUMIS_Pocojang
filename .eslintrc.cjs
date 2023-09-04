3532d39726bccebbd7028ef9bf2fc1c54d43dd6b
kutta97 <yhjin04170@gmail.com>
2023-09-04 16:10:30 +0900

    chore: import order 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,38 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: true,
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+        ],
+      },
+    ],
+    'import/no-unresolved': ['error', { ignore: ['.svg'] }],
   },
 };

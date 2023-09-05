c4e2a5e9f0fb51ca5baa43f9c86e5bf634109fda
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 00:24:08 +0900

    feat : project update페이지(수정페이지)

---
+++ src/pages/project/ProjectUpdate.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import { useParams } from 'react-router-dom';
+
+export default function ProjectUpdate() {
+  const { projectId } = useParams();
+  return (
+    <div>
+      <h1>{projectId}번 수정하는 페이지</h1>
+      <button>수정하기</button>
+    </div>
+  );
+}

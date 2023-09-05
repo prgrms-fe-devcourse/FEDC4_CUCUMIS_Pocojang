5ade257f07bfafd205314a27fab394be5e3ac2a4
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 00:20:31 +0900

    feat : developer detail페이지 생성

---
+++ src/pages/developer/DeveloperDetail.tsx
@@ -0,0 +1,17 @@
+import { Link, useParams } from 'react-router-dom';
+
+// import React from 'react'
+
+export default function DeveloperDetail() {
+  const { developerId } = useParams();
+
+  return (
+    <div>
+      <h1>{developerId}번 개발자!</h1>
+      <button>팔로우</button>
+      <Link to={`/dm/${developerId}`}>DM</Link>
+
+      <h2>댓글 다는 공간</h2>
+    </div>
+  );
+}

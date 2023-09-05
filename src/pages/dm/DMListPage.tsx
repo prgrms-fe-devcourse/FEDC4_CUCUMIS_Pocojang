4043d0d0c35d9df090794847904cb32e6d47327f
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 01:58:57 +0900

    feat : DMListpage생성

---
+++ src/pages/dm/DMListPage.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+// import React from 'react'
+
+export default function DMList() {
+  return (
+    <div>
+      <h1>DM List</h1>
+      <ul>
+        <li>
+          <Link to="/dm/노철">노철</Link>
+        </li>
+        <li>
+          <Link to="/dm/혜진">혜진</Link>
+        </li>
+        <li>
+          <Link to="/dm/원지">원지</Link>
+        </li>
+        <li>
+          <Link to="/dm/지성">지성</Link>
+        </li>
+        <li>
+          <Link to="/dm/승희">승희</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}

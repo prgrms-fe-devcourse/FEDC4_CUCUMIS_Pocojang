3f3fd20fa96343a751897ddf6a18ca72c330c651
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 01:56:20 +0900

    feat : extrainformation page생성

---
+++ src/pages/auth/ExtraInformationPage.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+import { Link } from 'react-router-dom';
+
+export default function ExtraInformationPage() {
+  return (
+    <div>
+      <h1>Extra Information Page</h1>
+      <button>
+        <Link to="/">건너뛰고 회원가입</Link>
+      </button>
+    </div>
+  );
+}

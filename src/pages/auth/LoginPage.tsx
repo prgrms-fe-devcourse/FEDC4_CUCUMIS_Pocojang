c4e5e9d844735225bec396878ab4b970e30c6c82
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 01:56:37 +0900

    feat : 로그인 페이지 생성

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+import { Link } from 'react-router-dom';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <p>
+          <input type="text" name="" id="" />
+        </p>
+        <p>
+          <input type="password" name="" id="" />
+        </p>
+        <button>로그인</button>
+        <button>
+          <Link to="/signup/step1">회원가입</Link>
+        </button>
+      </form>
+    </div>
+  );
+}

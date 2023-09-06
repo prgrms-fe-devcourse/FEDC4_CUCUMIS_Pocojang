058017e093b9dc55a6bcb455c8262308358d6240
eeseung <seunghee9941@gmail.com>
2023-09-06 15:18:26 +0900

    feat: palette 기본 색상 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,20 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFBE1E',
+      light: '#FFD685',
+      dark: '#FFA100',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#8EBF16',
+      light: '#A5CC45',
+      dark: '#7DA813',
+      contrastText: '#FFFFFF',
+    },
+  },
   typography: {
     fontFamily: "'Pretendard', sans-serif",
   },

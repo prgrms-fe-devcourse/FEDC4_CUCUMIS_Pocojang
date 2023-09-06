a2b08dc512395c9da1de91d3669476f4dce45008
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 13:24:22 +0900

    fix : import react 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes/appRouter';
 
-// import React from 'react'
-
 export default function App() {
   return <RouterProvider router={router} />;
 }

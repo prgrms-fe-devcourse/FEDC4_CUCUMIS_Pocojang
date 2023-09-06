7dbaec39b2c46791a6f6755af4d4bd6c5dc21ee3
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-06 16:51:28 +0900

    chore : 컴포넌트 폴더구조 변경

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from '@/components/header/header';
-import Nav from '@/components/nav/nav';
+import Header from '@/layout/HeaderLayout';
+import Nav from '@/components/nav';
 
 export default function RootLayout() {
   return (

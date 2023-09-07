5112e1aeeeb9e81b9ec6ff931867963834f03210
kutta97 <yhjin04170@gmail.com>
2023-09-07 17:21:04 +0900

    feat: authStore 에 setAuth 추가 - token, user 정보 저장

--- src/stores/auth/index.ts
+++ src/stores/auth/index.ts
@@ -2,5 +2,5 @@ import { authSlice } from '@/stores/auth/slice';
 import * as selector from '@/stores/auth/selector';
 
 export const authReducer = authSlice.reducer;
-export const { setIsLogin } = authSlice.actions;
+export const { setIsLogin, setAuth } = authSlice.actions;
 export const { authSelector } = selector;
--- src/stores/auth/slice.ts
+++ src/stores/auth/slice.ts
@@ -1,7 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { UserType } from '@/types';
+
 export interface AuthState {
   isLogin: boolean;
+  token?: string;
+  user?: UserType;
 }
 
 const initialState: AuthState = {
@@ -12,6 +16,12 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
+    setAuth: (state, action) => {
+      const { token, user } = action.payload;
+
+      state.token = token;
+      state.user = user;
+    },
     setIsLogin: (state, action) => {
       const { nextIsLogin } = action.payload;
       state.isLogin = nextIsLogin;

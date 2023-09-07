c6335130777a0bc26981af7f16f07b01005ecaa5
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-08 00:44:44 +0900

    refactor : 불필요한 types파일 삭제

--- src/api/auth/signup.ts
+++
@@ -1,8 +0,0 @@
-import { SignupRequestType, SignupResponseType } from '@/types/api/signup';
-import api from '@/utils/api';
-
-export const signup = async (
-  rq: SignupRequestType,
-): Promise<SignupResponseType> => {
-  return await api.post<SignupRequestType, SignupResponseType>('login', rq);
-};

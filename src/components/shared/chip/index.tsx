ae9520cadbafd771dc8bc42e85bb9e542c2ccce4
1g2g <wonji126326@gmail.com>
2023-09-07 10:06:18 +0900

    fix: chip 인터페이스에 키, 이벤트 타입 추가

--- src/components/shared/chip/index.tsx
+++ src/components/shared/chip/index.tsx
@@ -2,6 +2,8 @@ import Chip from '@mui/material/Chip';
 
 interface BasicChipProps {
   label: string;
+  key?: string;
+  onClick?: () => void;
 }
 const BasicChip = ({ label, ...props }: BasicChipProps) => {
   return <Chip label={label} {...props} />;

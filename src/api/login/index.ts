1410a255ce113e317667c119d8b4822cf8b4cd94
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-07 23:07:37 +0900

    refactor : api폴더 세팅 - 도메인 드라이븐 디자인 패턴을 응용한 폴더 세팅

---
+++ src/api/login/index.ts
@@ -0,0 +1,7 @@
+// import type { LoginRequestType, LoginResponseType } from "@/types/api/login";
+// import api from "@/utils/api";
+
+// export const login = async (rq: LoginRequestType): Promise<LoginResponseType> => {
+//   // return await api.post<LoginRequestType, LoginResponseType>("login", rq);
+// };
+// // 혜진님꺼 pull 받으면 실행될듯
---
+++ src/api/search/users/index.ts
@@ -0,0 +1,1 @@
+// users/ {query부분}

78f986ed8940e6d47a40fe043f07c4a52a082635
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-08 02:20:26 +0900

    refactor : 리팩토링 된 login type파일 import

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,10 +1,9 @@
-import { LoginRequestType } from '@/types/api/login';
+import type { RequestLoginType } from "../types/api/login/RequestLoginType"
 
 export default function useAuth() {
-  const login = async (loginRequest: LoginRequestType) => {
+  const login = async (loginRequest: RequestLoginType) => {
     const rs = await login(loginRequest); // TODO : ❗️CHECK => HOOK으로❗️
     console.log(rs);
-    // state.isLogin = ;
   };
 
   return {

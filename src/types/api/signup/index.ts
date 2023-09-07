34c5b73cac43ae51c3e08ec14ad2bca675277792
kutta97 <yhjin04170@gmail.com>
2023-09-07 16:55:58 +0900

    refactor: login, signup response, request type 위치 수정

--- src/types/api/signup/index.ts
+++ src/types/api/signup/index.ts
@@ -1,8 +1,9 @@
-import type { LoginResponseType } from '../login';
+import { LoginResponseType } from '@/types/api/login';
+
 export interface SignupRequestType {
   email: string;
   fullName: string;
   password: string;
 }
 
-export interface SignupType extends LoginResponseType {}
+export interface SignupResponseType extends LoginResponseType {}

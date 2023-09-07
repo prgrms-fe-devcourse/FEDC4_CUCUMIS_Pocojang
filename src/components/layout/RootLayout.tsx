ff8fcd0077796a1d9f01197b3c61d53dc4180380
kutta97 <yhjin04170@gmail.com>
2023-09-07 16:54:27 +0900

    feat: RootLayout Provider tag wrapping 추가

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,18 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 
 import Header from '@/components/layout/HeaderLayout';
 import Nav from '@/components/nav';
 import { theme } from '@/styles/theme';
+import { store } from '@/stores';
 
 export default function RootLayout() {
   return (
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <div>
-          <Header />
-          <Outlet />
-          <Nav />
-        </div>
+        <Header />
+        <Outlet />
+        <Nav />
       </ThemeProvider>
+    </Provider>
   );
 }

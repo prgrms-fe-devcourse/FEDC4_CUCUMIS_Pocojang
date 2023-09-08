10ec279ccd13ed0c1408b7dabd2fc3aeb1d7e502
nocheol <qkdl60@gmail.com>
2023-09-08 17:01:14 +0900

    refactor: BasicButton props 수정

--- src/components/shared/button/index.tsx
+++ src/components/shared/button/index.tsx
@@ -2,25 +2,11 @@ import Button, { ButtonProps } from '@mui/material/Button';
 
 interface Props extends ButtonProps {
   children: React.ReactNode;
-  name: string;
-  onClick: () => void;
 }
 
-const BasicButton = ({
-  variant = 'contained',
-  name,
-  onClick,
-  children,
-  ...props
-}: Props) => {
+const BasicButton = ({ variant = 'contained', children, ...props }: Props) => {
   return (
-    <Button
-      fullWidth
-      variant={variant}
-      name={name}
-      onClick={onClick}
-      {...props}
-    >
+    <Button fullWidth variant={variant} {...props}>
       {children}
     </Button>
   );

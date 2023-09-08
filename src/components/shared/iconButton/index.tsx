ca7df605f3288baeb397e67bf75bdc20654d6414
nocheol <qkdl60@gmail.com>
2023-09-08 16:33:50 +0900

    feat: basic-icon-button 구현

---
+++ src/components/shared/iconButton/index.tsx
@@ -0,0 +1,14 @@
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+
+interface Props extends IconButtonProps {
+  children: React.ReactNode;
+}
+const BasicIconButton = ({ children, color = 'primary', ...props }: Props) => {
+  return (
+    <IconButton color={color} {...props}>
+      {children}
+    </IconButton>
+  );
+};
+
+export default BasicIconButton;

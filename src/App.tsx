603b0fcddc46bf82684a48a39111855c3a25647b
kutta97 <yhjin04170@gmail.com>
2023-09-08 16:52:09 +0900

    feat: App 에 redux listener 추가 및 해제 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
+import { useEffect } from 'react';
+import type { Unsubscribe } from '@reduxjs/toolkit';
+
+import { setupListeners } from '@/stores/listeners';
 
 import { router } from './routes/appRouter';
 
 export default function App() {
+  useEffect(() => {
+    const subscriptions: Unsubscribe[] = setupListeners();
+    return () => subscriptions.forEach((unsubscribe) => unsubscribe());
+  }, []);
+
   return (
     <>
       <RouterProvider router={router} />

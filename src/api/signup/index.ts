4242ed532672a08e7d01e1ca8470f979b205448e
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-08 13:49:56 +0900

    feat : signup api호출

--- src/api/signup/index.ts
+++ src/api/signup/index.ts
@@ -1,7 +1,7 @@
-import { SignupRequestType, SignupResponseType } from "@/types/api/signup";
+import type { RequestSignupType, ResponseSignupType } from "@/types/api/signup";
 import api from "@/utils/api";
 export const signup = async (
-  rq: SignupRequestType,
-): Promise<SignupResponseType> => {
-  return await api.post<SignupRequestType, SignupResponseType>("login", rq);
+  rq: RequestSignupType,
+): Promise<ResponseSignupType> => {
+  return await api.post<RequestSignupType, ResponseSignupType>("login", rq);
 };

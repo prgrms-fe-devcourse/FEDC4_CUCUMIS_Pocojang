02f4367c75f5090aeb3cffef421b6d0891414401
nocheol <qkdl60@gmail.com>
2023-09-10 12:26:32 +0900

    refactor: 아이콘 버트 children 타입 수정
    -아이콘 버튼 children이 아이콘컴포넌트만 받도록 수정

--- src/components/shared/iconButton/index.tsx
+++ src/components/shared/iconButton/index.tsx
@@ -1,8 +1,11 @@
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import SvgIcon from '@mui/material/SvgIcon';
 
 interface Props extends IconButtonProps {
-  children: React.ReactNode;
+  children: React.ReactElement<SvgIconComponent>;
 }
+
+type SvgIconComponent = typeof SvgIcon;
 
 const BasicIconButton = ({ children, color = 'primary', ...props }: Props) => {
   return (

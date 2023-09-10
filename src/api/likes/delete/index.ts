d8aaba150149db562c3e5a23208f2fecd47425f1
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-10 13:14:31 +0900

    refactor : likes api생성

---
+++ src/api/likes/delete/index.ts
@@ -0,0 +1,7 @@
+import type { RequestBodyLikesDeleteType } from "@/types/api/likes/delete/RequestBodyDeleteLikesType";
+import type { LikeType } from "@/types";
+import api from "@/utils/api";
+
+export const deleteLikes = async (rq : RequestBodyLikesDeleteType): Promise<LikeType> => {
+  return await api.del<RequestBodyLikesDeleteType, LikeType>("/likes/delete", rq);
+};

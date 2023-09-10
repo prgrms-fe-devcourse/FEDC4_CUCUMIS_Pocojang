1591651ba4b6a4c7923f47fbbbd9d6e47f011744
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-10 13:15:48 +0900

    refactor : search api생성

--- src/api/search/all/index.ts
+++ src/api/search/all/index.ts
@@ -1,1 +1,7 @@
 // query
+
+import api from "@/utils/api";
+import type { UserType, PostType } from "@/types";
+export const searchPost_User = async (): Promise<UserType | PostType> => {
+  return await api.get<null, UserType | PostType>("/search/all/{query}");
+};
--- src/api/search/users/index.ts
+++ src/api/search/users/index.ts
@@ -1,1 +1,6 @@
 // users/ {query부분}
+import api from "@/utils/api";
+import type { UserType } from "@/types";
+export const searchUsers = async (): Promise<UserType> => {
+  return await api.get<null, UserType>("/search/users/{query}");
+};

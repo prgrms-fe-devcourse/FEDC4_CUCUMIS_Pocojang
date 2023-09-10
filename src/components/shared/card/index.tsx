306c41187d8a49b99766d4cf2677455708fd02a4
nocheol <qkdl60@gmail.com>
2023-09-10 16:18:14 +0900

    feat: BasicCard 컴포넌트 구현

---
+++ src/components/shared/card/index.tsx
@@ -0,0 +1,21 @@
+import Card, { CardProps } from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import styled from '@emotion/styled';
+
+const BasicCard = ({ children, ...props }: CardProps) => {
+  return (
+    <StyledCard {...props}>
+      <CardActionArea>{children}</CardActionArea>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled(Card)(() => ({
+  '&.MuiCard-root': {
+    transition: 'transform .2s ',
+    '&:hover': {
+      transform: 'scale(1.01)',
+    },
+  },
+}));
+export default BasicCard;

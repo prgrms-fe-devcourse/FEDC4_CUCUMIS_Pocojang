3f5286be2b214e08da4bcada8b4e4c811f2ee672
eeseung <seunghee9941@gmail.com>
2023-09-11 00:41:21 +0900

    feat: RootLayout location dispatch 추가

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,11 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 
 import Header from '@/components/layout/HeaderLayout';
 import Navbar from '@/components/navbar';
 import { theme } from '@/styles/theme';
+import { setLocation } from '@/stores/layout';
+import { useAppDispatch } from '@/stores/hooks';
 
 export default function RootLayout() {
+  const dispatch = useAppDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(setLocation({ nextLocation: location.pathname }));
+  }, [dispatch, location]);
+
   return (
     <ThemeProvider theme={theme}>
       <Header />

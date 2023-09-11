ea65d63cad2e59b1fd9f774309211f3444e9ea93
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-09-11 16:10:55 +0900

    Feature/38 cardComponent 구현 (#46)
    * feat: projectCard 컴포넌트 구현
    
    * feat: BasicCard 컴포넌트 구현
    
    * feat: projectCardComponent 구현
    
    * feat: developerCardComponent 구현
    
    * feat: card 사이즈 수정
    
    * refactor: card component들 사이즈 및 폰트 사이즈 수정
    
    * feat: 클릭시 이동 기능 추가
    
    * refactor : 컴포넌트이름변경
    
    -Card에서 CardItem으로 이름변경

---
+++ src/components/shared/card/index.tsx
@@ -0,0 +1,33 @@
+import Card, { CardProps } from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+interface Props extends CardProps {
+  to: string;
+}
+
+const BasicCard = ({ children, to }: Props) => {
+  return (
+    <StyledLink to={to}>
+      <StyledCard raised={true}>
+        <CardActionArea>{children}</CardActionArea>
+      </StyledCard>
+    </StyledLink>
+  );
+};
+
+const StyledCard = styled(Card)(() => ({
+  '& ': {
+    width: '350px',
+    transition: 'transform .2s ',
+    '&:hover': {
+      transform: 'scale(1.01)',
+    },
+  },
+}));
+
+const StyledLink = styled(Link)(() => ({
+  textDecoration: 'none',
+}));
+export default BasicCard;
---
+++ src/components/shared/developerCard/index.tsx
@@ -0,0 +1,55 @@
+import { CardContent, Grid, Typography } from '@mui/material';
+
+import type BasicAvatarProps from '@/types/components/BasicAvatarProps';
+import BasicAvatar from '@/components/shared/avatar';
+import BasicChip from '@/components/shared/chip';
+import ChipGroup from '@/components/shared/chipGroup';
+import BasicCard from '@/components/shared/card';
+
+interface Props {
+  AvatarProps: BasicAvatarProps;
+  oneliner: string;
+  name: string;
+  description: string;
+  to: string;
+}
+
+//TODO 스택은 반복문으로 변경하기,
+const DeveloperCardItem = ({
+  AvatarProps,
+  oneliner,
+  name,
+  description,
+  to,
+}: Props) => {
+  return (
+    <BasicCard to={`/developers/${to}`}>
+      <CardContent>
+        <Grid container alignItems="center">
+          <Grid item xs={3}>
+            <BasicAvatar {...AvatarProps} />
+          </Grid>
+          <Grid item xs={9}>
+            <Typography variant="body1" component="p" noWrap={true}>
+              {oneliner}
+            </Typography>
+            <Typography variant="body1" component="p">
+              {name}
+            </Typography>
+            <ChipGroup>
+              <BasicChip color="primary" label="test1"></BasicChip>
+              <BasicChip color="primary" label="test2"></BasicChip>
+              <BasicChip color="primary" label="test3"></BasicChip>
+            </ChipGroup>
+          </Grid>
+        </Grid>
+
+        <Typography variant="body1" component="p" noWrap={true}>
+          {description}
+        </Typography>
+      </CardContent>
+    </BasicCard>
+  );
+};
+
+export default DeveloperCardItem;

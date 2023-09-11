222465c981fd82e27e2ab2a6bddf415daee69653
kutta97 <yhjin04170@gmail.com>
2023-09-11 18:47:17 +0900

    feat: signup step2 페이지 추가

--- src/pages/signup/step2.tsx
+++ src/pages/signup/step2.tsx
@@ -1,12 +1,27 @@
-import { Link } from 'react-router-dom';
+import BasicInput from '@/components/shared/input';
+import SignupInputContainer from '@/components/signup/signupInputContainer';
 
 export default function ExtraInformationPage() {
+  const handleClickSignup = () => {};
+
   return (
-    <div>
-      <h1>Extra Information Page</h1>
-      <button>
-        <Link to="/">건너뛰고 회원가입</Link>
-      </button>
-    </div>
+    <SignupInputContainer
+      buttonText="건너뛰고 회원가입"
+      onSubmit={handleClickSignup}
+    >
+      <BasicInput
+        type="search"
+        label="One liner"
+        regExp="[a-z0-9]+@[a-z]+.[a-z]{2,3}"
+        errorMessage="몇 자 이상 validation"
+      />
+      <BasicInput
+        type="search"
+        label="Technical Tools(option)"
+        errorMessage="기술 스택 리스트"
+      />
+      <BasicInput type="password confirm" label="Technical Position(option)" />
+      <BasicInput type="name" label="Details(option)" />
+    </SignupInputContainer>
   );
 }

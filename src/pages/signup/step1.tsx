6fb66b627b531dcf217f5eb304c773271cc75651
kutta97 <yhjin04170@gmail.com>
2023-09-11 18:47:01 +0900

    feat: signup step1 페이지 추가

--- src/pages/signup/step1.tsx
+++ src/pages/signup/step1.tsx
@@ -1,13 +1,40 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import BasicInput from '@/components/shared/input';
+import SignupInputContainer from '@/components/signup/signupInputContainer';
 
 export default function SignupPage() {
+  const navigate = useNavigate();
+  const handleClickNext = () => {
+    navigate('/signup/step2');
+  };
+
   return (
-    <div>
-      <h1>sign up</h1>
-      <input type="email" name="" id="" />
-      <p>
-        <Link to="/signup/step2">다음</Link>
-      </p>
-    </div>
+    <SignupInputContainer buttonText="다음" onSubmit={handleClickNext}>
+      <BasicInput
+        label="email"
+        regExp="[a-z0-9]+@[a-z]+.[a-z]{2,3}"
+        errorMessage="영문, 숫자 포함 몇 자 이상 validation"
+        isRequired
+      />
+      <BasicInput
+        type="password"
+        label="password"
+        regExp="^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$"
+        errorMessage="영문, 숫자 포함 몇 자 이상 validation"
+        isRequired
+      />
+      <BasicInput
+        label="password confirm"
+        errorMessage="영문, 숫자 포함 몇 자 이상 validation"
+        isRequired
+      />
+      <BasicInput
+        label="name"
+        regExp=""
+        errorMessage="영문, 숫자 포함 몇 자 이상 validation"
+        isRequired
+      />
+    </SignupInputContainer>
   );
 }

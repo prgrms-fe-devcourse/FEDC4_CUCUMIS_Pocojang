9fdb33582faf3e984bf9286ac1de35fafecf6077
1g2g <wonji126326@gmail.com>
2023-09-12 23:15:40 +0900

    test

--- src/pages/developers/[developerId].tsx
+++ src/pages/developers/[developerId].tsx
@@ -8,7 +8,6 @@ export default function DeveloperDetail() {
       <h1>{developerId}번 개발자!</h1>
       <button>팔로우</button>
       <Link to={`/dm/${developerId}`}>DM</Link>
-
       <h2>댓글 다는 공간</h2>
     </div>
   );

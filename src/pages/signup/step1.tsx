adb0bbfd8b7143b8e62156ae84272d8789be945a
Hyejin Yang <yhjin04170@gmail.com>
2023-09-12 21:41:27 +0900

    Feature/52 signup page (#69)
    * feat: 로고 asset 추가
    
    * feat: signupInputContainer 추가
    - step1, step2 의 공통 UI 부분을 컴포넌트로 분리
    
    * feat: signup step1 페이지 추가
    
    * feat: signup step2 페이지 추가
    
    * feat: useForm 에서 FormValues, FormErrors 타입 export
    
    * feat: TextField 컴포넌트에 누락된 label 추가
    
    * feat: signupInputContainer onSubmit 함수 타입 수정
    
    * feat: step1, step2 페이지에 변경된 BaseInput 컴포넌트 적용
    
    * feat: RootLayout 컴포넌트 에 맞춰 padding 조정
    
    * feat: 회원가입 input validations 추가
    
    * feat: step1, step2 validation 적용
    
    * feat: signup page layout 수정
    
    * feat: signup page step2 input required 옵션 false 로 수정

--- src/pages/signup/step1.tsx
+++ src/pages/signup/step1.tsx
@@ -1,13 +1,80 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import useForm, { FormErrors, FormValues } from '@/hooks/components/useForm';
+import SignupInputContainer from '@/components/signup/signupInputContainer';
+import BasicInput from '@/components/shared/input';
+import validation from '@/utils/validations';
+
+const initialValues = {
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  name: '',
+};
+
+const validate = ({ email, password, passwordConfirm, name }: FormValues) => {
+  const newErrors: FormErrors = {};
+
+  const emailErrorMessage = validation.email(email);
+  const passwordErrorMessage = validation.email(password);
+  const passwordConfirmErrorMessage = validation.email(passwordConfirm);
+  const nameErrorMessage = validation.email(name);
+
+  if (emailErrorMessage) newErrors.email = emailErrorMessage;
+  if (passwordErrorMessage) newErrors.password = passwordErrorMessage;
+  if (passwordConfirmErrorMessage)
+    newErrors.passwordConfirm = passwordConfirmErrorMessage;
+  if (nameErrorMessage) newErrors.name = nameErrorMessage;
+
+  return newErrors;
+};
 
 export default function SignupPage() {
+  const navigate = useNavigate();
+  const onSubmit = async (value: FormValues) => {
+    // TODO: signup store 에 value 저장
+    console.log(value);
+
+    navigate('/signup/step2');
+  };
+
+  const { errors, handleChange, handleSubmit } = useForm({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
   return (
-    <div>
-      <h1>sign up</h1>
-      <input type="email" name="" id="" />
-      <p>
-        <Link to="/signup/step2">다음</Link>
-      </p>
-    </div>
+    <SignupInputContainer buttonText="다음" onSubmit={handleSubmit}>
+      <BasicInput
+        label="email"
+        placeholder="이메일을 입력해주세요"
+        onChange={handleChange}
+        errorMessage={errors.email}
+        isRequired
+      />
+      <BasicInput
+        type="password"
+        label="password"
+        placeholder="비밀번호를 입력해주세요"
+        onChange={handleChange}
+        errorMessage={errors.password}
+        isRequired
+      />
+      <BasicInput
+        label="password confirm"
+        placeholder="비밀번호를 입력해주세요"
+        onChange={handleChange}
+        errorMessage={errors.passwordConfig}
+        isRequired
+      />
+      <BasicInput
+        label="name"
+        placeholder="이름을 입력해주세요"
+        onChange={handleChange}
+        errorMessage={errors.name}
+        isRequired
+      />
+    </SignupInputContainer>
   );
 }
--- src/pages/signup/step2.tsx
+++ src/pages/signup/step2.tsx
@@ -1,12 +1,76 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import useForm, { FormErrors, FormValues } from '@/hooks/components/useForm';
+import BasicInput from '@/components/shared/input';
+import SignupInputContainer from '@/components/signup/signupInputContainer';
+
+const initialValues = {
+  oneLiner: '',
+  technicalTools: '',
+  position: '',
+  details: '',
+};
+
+const validate = ({
+  oneLiner,
+  technicalTools,
+  position,
+  details,
+}: FormValues): FormErrors => {
+  const newErrors: FormErrors = {};
+  if (!oneLiner) newErrors.oneLiner = 'please enter oneLiner';
+  if (!technicalTools) newErrors.technicalTools = 'please enter technicalTools';
+  if (!position) newErrors.position = 'please enter position';
+  if (!details) newErrors.details = 'please enter details';
+  return newErrors;
+};
 
 export default function ExtraInformationPage() {
+  const navigate = useNavigate();
+  const onSubmit = async (value: FormValues) => {
+    console.log(value);
+    navigate('/home');
+  };
+
+  const { errors, handleChange, handleSubmit } = useForm({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
   return (
-    <div>
-      <h1>Extra Information Page</h1>
-      <button>
-        <Link to="/">건너뛰고 회원가입</Link>
-      </button>
-    </div>
+    <SignupInputContainer
+      buttonText="건너뛰고 회원가입"
+      onSubmit={handleSubmit}
+    >
+      <BasicInput
+        label="One Liner"
+        placeholder="한 줄 포부를 작성해주세요"
+        onChange={handleChange}
+        errorMessage={errors.oneLiner}
+        isRequired={false}
+      />
+      <BasicInput
+        label="Technical Tools"
+        placeholder="기술 스택을 추가해주세요"
+        onChange={handleChange}
+        errorMessage={errors.technicalTools}
+        isRequired={false}
+      />
+      <BasicInput
+        label="Position"
+        placeholder="직군을 입력해주세요"
+        onChange={handleChange}
+        errorMessage={errors.position}
+        isRequired={false}
+      />
+      <BasicInput
+        label="Details"
+        placeholder="소개글을 작성해주세요"
+        onChange={handleChange}
+        errorMessage={errors.details}
+        isRequired={false}
+      />
+    </SignupInputContainer>
   );
 }

ef5787d5aa52edf38428712adb9313917b3278e1
nocheol <qkdl60@gmail.com>
2023-09-12 23:14:32 +0900

    refactor: basicFab 와 mainFab 분리

--- src/components/shared/fab/index.tsx
+++ src/components/shared/fab/index.tsx
@@ -1,54 +1,15 @@
 import Fab, { FabProps } from '@mui/material/Fab';
-import { Box } from '@mui/material';
-import styled from '@emotion/styled';
 
 interface Props extends FabProps {
-  bottom?: number;
-  right?: number;
   children: React.ReactNode;
-  onClick: () => void;
 }
 
-const BasicFab = ({
-  bottom = 80,
-  right = 20,
-  children,
-  onClick,
-  ...props
-}: Props) => {
+const BasicFab = ({ children, ...props }: Props) => {
   return (
-    <BoxStyled>
-      <FabStyled
-        bottom={bottom}
-        right={right}
-        color="primary"
-        onClick={onClick}
-        {...props}
-      >
-        {children}
-      </FabStyled>
-    </BoxStyled>
+    <Fab color="primary" {...props}>
+      {children}
+    </Fab>
   );
 };
 
-const FabStyled = styled(Fab)<{ bottom: number; right: number }>(
-  ({ bottom, right }) => ({
-    position: 'absolute',
-    bottom: `${bottom}px`,
-    right: `${right}px`,
-    pointerEvents: 'auto',
-  }),
-);
-
-const BoxStyled = styled(Box)({
-  zIndex: '10',
-  maxWidth: '600px',
-  bottom: '0',
-  width: '100%',
-  height: 'calc(100vh - 80px)',
-  position: 'fixed',
-  left: '50%',
-  transform: 'translate(-50%)',
-  pointerEvents: 'none',
-});
 export default BasicFab;

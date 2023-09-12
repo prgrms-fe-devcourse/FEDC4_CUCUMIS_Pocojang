1260c22a90d43b9132d81f4179e6e06be797b306
kutta97 <yhjin04170@gmail.com>
2023-09-13 03:19:02 +0900

    refactor: signup/step1 페이지에 useSignupForm 커스텀 hook 추가

---
+++ src/components/signup/useSignupForm.ts
@@ -0,0 +1,65 @@
+import { signup } from '@/api/auth/signup';
+import useForm, { FormErrors, FormValues } from '@/hooks/components/useForm';
+import { ResponseSignupType } from '@/types/api/signup';
+import validation from '@/utils/validations';
+
+const initialSignupFormValues = {
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  name: '',
+};
+
+const validateSignupForm = ({
+  email,
+  password,
+  passwordConfirm,
+  name,
+}: FormValues) => {
+  const newErrors: FormErrors = {};
+
+  const emailErrorMessage = validation.email(email);
+  const passwordErrorMessage = validation.email(password);
+  const passwordConfirmErrorMessage = validation.email(passwordConfirm);
+  const nameErrorMessage = validation.email(name);
+
+  if (emailErrorMessage) newErrors.email = emailErrorMessage;
+  if (passwordErrorMessage) newErrors.password = passwordErrorMessage;
+  if (passwordConfirmErrorMessage)
+    newErrors.passwordConfirm = passwordConfirmErrorMessage;
+  if (nameErrorMessage) newErrors.name = nameErrorMessage;
+
+  return newErrors;
+};
+
+interface SignupFormHookParameters {
+  onSuccess: (rs: ResponseSignupType) => void;
+  onFail: (error: unknown) => void;
+}
+
+export const useSignupForm = ({
+  onSuccess,
+  onFail,
+}: SignupFormHookParameters) => {
+  const onSubmitSignupForm = async ({ email, password, name }: FormValues) => {
+    try {
+      const rs = await signup({ email, password, fullName: name });
+      onSuccess(rs);
+    } catch (error) {
+      // TODO: 회원가입 실패 알림
+      onFail(error);
+    }
+  };
+
+  const { errors, handleChange, handleSubmit } = useForm({
+    initialValues: initialSignupFormValues,
+    onSubmit: onSubmitSignupForm,
+    validate: validateSignupForm,
+  });
+
+  return {
+    signupFormErrors: errors,
+    handleSignupFormChange: handleChange,
+    handleSignupFormSubmit: handleSubmit,
+  };
+};

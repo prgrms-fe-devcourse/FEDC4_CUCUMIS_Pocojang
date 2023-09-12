e3dde652eb7c0c7ce8d5046c06a4429b67144f4a
1g2g <wonji126326@gmail.com>
2023-09-13 02:21:22 +0900

    fix: type명 변경

--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -2,7 +2,7 @@ import { List } from '@mui/material';
 
 import ItemWithAvatar from '@/components/shared/itemWithAvatar';
 
-interface CommentProps {
+type Comment = {
   _id: string;
   author: string;
   createdAt: string;
@@ -11,10 +11,10 @@ interface CommentProps {
     isUserOn: boolean;
   };
   isLastItem?: boolean;
-}
+};
 
 interface Props {
-  comments: CommentProps[];
+  comments: Comment[];
   url: string;
   onClick: (url: string, _id: string) => void;
 }

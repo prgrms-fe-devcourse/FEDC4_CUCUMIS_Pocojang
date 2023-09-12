4d7b710e8fc95e08e9d98a88d048474c4008b088
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-12 22:58:03 +0900

    더미데이터 폴더 변경

---
+++ src/components/profile/useProfileData.ts
@@ -0,0 +1,109 @@
+const DUMMY_DATA = {
+  LIST_DUMMY_DATA: [
+    { label: 320, title: '팔로잉' },
+    { label: 321, title: '팔로워' },
+    { label: 10, title: '포스트' },
+    { label: 7, title: '스크랩' },
+  ],
+  FOLLOWING_DUMMY_DATA: [
+    {
+      userName: '유저1',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저2',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저3',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저4',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+  ],
+  FOLLOWER_DUMMY_DATA: [
+    {
+      userName: '유저1',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저2',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저3',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저4',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저5',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저6',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저7',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저8',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+    {
+      userName: '유저9',
+      userImgUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+    },
+  ],
+  CARD_DUMMY_DATA: [
+    {
+      name: '지성1',
+      imageUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+      to: '1',
+      projectTitle: 'project1',
+    },
+    {
+      name: '지성2',
+      imageUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+      to: '2',
+      projectTitle: 'project2',
+    },
+    {
+      name: '지성3',
+      imageUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+      to: '3',
+      projectTitle: 'project3',
+    },
+    {
+      name: '지성4',
+      imageUrl:
+        'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg',
+      to: '4',
+      projectTitle: 'project4',
+    },
+  ],
+};
+
+export default DUMMY_DATA;

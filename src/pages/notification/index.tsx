d67332ece61d2a56cc1eb3eb5016d8691bcba4cd
nocheol <qkdl60@gmail.com>
2023-09-13 05:13:59 +0900

    feat: useNotification 구현 및 적용

--- src/pages/notification/index.tsx
+++ src/pages/notification/index.tsx
@@ -1,85 +1,26 @@
-import {
-  Stack,
-  Typography,
-  Card,
-  CardContent,
-  Divider,
-  List,
-} from '@mui/material';
-import { Circle } from '@mui/icons-material';
-import styled from '@emotion/styled';
+import { Stack } from '@mui/material';
 
-import { theme } from '@/styles/theme';
+import NotificationItem from '@/components/notification/NotificationItem';
+import useNotifications from '@/components/notification/useNotification';
 
-//TODO key는 _id로 변경, 이벤트 바인딩
 const NotificationPage = () => {
+  //TODO 클릭시 읽음 처리
+  const { notificationMessage, notifications } = useNotifications();
   return (
-    <List>
-      {dummyNotifications.map((notification, index) => (
-        <>
-          <NotificationItem key={index} seen={notification.seen}>
-            {notification.name + notificationMessage[notification.type]}
-          </NotificationItem>
-          {dummyNotifications.length - 1 !== index ? <StyledDivider /> : ''}
-        </>
+    <Stack spacing={2}>
+      {notifications.map(({ _id, type, seen, name }) => (
+        <NotificationItem
+          onClick={() => {
+            console.log('읽음 처리');
+          }}
+          key={_id}
+          seen={seen}
+        >
+          {name + notificationMessage[type]}
+        </NotificationItem>
       ))}
-    </List>
+    </Stack>
   );
 };
 
 export default NotificationPage;
-interface Props {
-  seen: boolean;
-  children: React.ReactNode;
-}
-const NotificationItem = ({ seen, children }: Props) => {
-  return (
-    <StyledCard
-      seen={seen}
-      raised={false}
-      square={true}
-      onClick={() => console.log('hi')}
-    >
-      <CardContent component={Stack} direction="row" alignItems="center">
-        <Typography noWrap={true} flexGrow={1} variant="body1">
-          {children}
-        </Typography>
-        {seen ? '' : <Circle color="primary" />}
-      </CardContent>
-    </StyledCard>
-  );
-};
-
-const StyledCard = styled(Card, {
-  shouldForwardProp: (prop) => prop !== 'seen',
-})(({ seen }: { seen: boolean }) => ({
-  backgroundColor: seen ? '' : `${theme.palette.primary.main}10`,
-  boxShadow: 'none',
-  '.MuiCardContent-root': {
-    paddingTop: '24px',
-  },
-}));
-
-const StyledDivider = styled(Divider)(() => ({
-  color: 'transparent',
-  margin: '3px auto',
-}));
-const notificationMessage: { [key: string]: string } = {
-  COMMENT: '님이 포스트에 댓글을 작성했습니다',
-  MESSAGE: '님이 메세지를 보냈습니다',
-  LIKE: '님이 포스트에 좋아요',
-  FOLLOW: '님이 팔로우 했습니다',
-};
-
-const dummyNotifications = [
-  { type: 'COMMENT', seen: true, name: 'user1' },
-  { type: 'MESSAGE', seen: true, name: 'user2' },
-  { type: 'FOLLOW', seen: false, name: 'user3' },
-  { type: 'COMMENT', seen: false, name: 'user4' },
-  { type: 'MESSAGE', seen: true, name: 'user5' },
-  { type: 'LIKE', seen: true, name: 'user6' },
-  { type: 'COMMENT', seen: false, name: 'user7' },
-  { type: 'LIKE', seen: true, name: 'user8' },
-  { type: 'COMMENT', seen: true, name: 'user9' },
-  { type: 'MESSAGE', seen: true, name: 'user10' },
-];

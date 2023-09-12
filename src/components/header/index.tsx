dd8479641ee0e094ea42ca76cec7ad6d6b3f87cd
Seunghee Lee <49032882+eeseung@users.noreply.github.com>
2023-09-12 16:47:22 +0900

    Feature/20 header component (#63)
    * chore: HeaderLayout 삭제
    
    * feat: layoutStore input state 추가
    
    * feat: layoutStore setInput action 추가
    
    * feat: HeaderType 타입 추가
    
    * chore: authStore selector export 추가
    
    * feat: layoutStore headerType selector 추가
    
    * feat: getHeaderType util 함수 구현
    
    * feat: Title 타입 추가
    
    * feat: getTitle util 함수 구현
    
    * feat: BasicSearch UI 수정
    
    * feat: getHeaderType, getTitle util 함수 수정
    
    - /profile 비로그인인 경우 프로필 로고 헤더
    
    * feat: Header 컴포넌트 구현
    
    * feat: location 변경 시 layoutStore input 초기화
    
    * chore: layoutStore selector export 추가
    
    * feat: Navbar 컴포넌트 input 구현
    
    * feat: Header 컴포넌트 UI 수정
    
    * feat: RootLayout main 태그 추가 및 UI 수정
    
    * feat: authStore initialState 수정
    
    - 임시 userId 제거
    
    * feat: BasicSearch placeholder 기본값 제거
    
    - Header, Navbar에서 prop 전달
    
    * feat: Header, Navbar - BasicSearch useForm 반영
    
    * fix: Navbar input 보이는 조건 수정
    
    * feat: Header 설정 타이틀 UI 수정

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,150 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
-export default function Header() {
+import { useAppDispatch, useAppSelector } from '@/stores/hooks';
+import { setInput, headerTypeSelector, titleSelector } from '@/stores/layout';
+import { isLoginSelector } from '@/stores/auth';
+import { HeaderType, Title } from '@/types/components/Header';
+import useForm from '@/hooks/components/useForm';
+import BasicChip from '@/components/shared/chip';
+import BasicSearch from '@/components/shared/search';
+import BasicIconButton from '@/components/shared/iconButton';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const headerType = useAppSelector(headerTypeSelector);
+  const title = useAppSelector(titleSelector);
+  const isLogin = useAppSelector(isLoginSelector);
+  const { handleChange, handleSubmit } = useForm({
+    initialValues: { search: '' },
+    onSubmit: ({ search }) => dispatch(setInput(search)),
+    validate: ({ search }) => {
+      const newErrors = { search: '' };
+      if (!search) newErrors.search = '검색어를 입력해주세요.';
+      return newErrors;
+    },
+  });
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goToLogin = () => {
+    navigate('/login');
+  };
+
+  const logout = () => {
+    // TODO: 로그아웃
+  };
+
   return (
-    <header>
-      <Link to="/login">로그인</Link>
-      <Link to="/notification">알림!</Link>
-    </header>
+    <AppBarStyled position="fixed" elevation={0}>
+      <ToolbarStyled>
+        {headerType === HeaderType.LOGO && (
+          <>
+            <LinkStyled to="/">
+              <img src="/assets/Logo512.png" alt="CUCUMIS" width={40} />
+            </LinkStyled>
+            <TypographyStyled variant="h6" component="h1" color="primary">
+              {title}
+            </TypographyStyled>
+            {isLogin ? (
+              <Link to="/notification">
+                <NotificationsIcon color="primary" />
+              </Link>
+            ) : (
+              <BasicChip
+                label="로그인"
+                variant="outlined"
+                color="primary"
+                onClick={goToLogin}
+              />
+            )}
+          </>
+        )}
+        {headerType === HeaderType.SEARCH && (
+          <>
+            <LinkStyled to="/">
+              <img src="/assets/Logo512.png" alt="CUCUMIS" width={40} />
+            </LinkStyled>
+            <FormStyled onSubmit={handleSubmit}>
+              <BasicSearch
+                onChange={handleChange}
+                placeholder="검색어를 입력해주세요"
+              />
+              <BasicIconButton type="submit">
+                <SearchIcon />
+              </BasicIconButton>
+            </FormStyled>
+          </>
+        )}
+        {headerType === HeaderType.BACK && (
+          <>
+            <BasicIconButton onClick={goBack}>
+              <ArrowBackIosNewIcon />
+            </BasicIconButton>
+            <TypographyStyled
+              variant="h6"
+              component="h1"
+              color="primary"
+              align="center"
+              paddingRight={title === Title.SETTINGS && isLogin ? 0 : 40}
+            >
+              {title}
+            </TypographyStyled>
+            {title === Title.SETTINGS && isLogin && (
+              <BasicChip
+                label="로그아웃"
+                variant="outlined"
+                color="primary"
+                onClick={logout}
+              />
+            )}
+          </>
+        )}
+      </ToolbarStyled>
+    </AppBarStyled>
   );
-}
+};
+
+const AppBarStyled = styled(AppBar)({
+  maxWidth: '600px',
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+  backgroundColor: 'white',
+  boxShadow: '0 4px 4px 0 rgba(0, 0, 0, 0.04)',
+});
+
+const ToolbarStyled = styled(Toolbar)({
+  paddingRight: '8px',
+  paddingLeft: '8px',
+});
+
+const LinkStyled = styled(Link)`
+  display: flex;
+  padding-right: 12px;
+`;
+
+const TypographyStyled = styled(Typography)<{
+  align: string;
+  paddingRight: number;
+}>(({ align, paddingRight }) => ({
+  flexGrow: 1,
+  textAlign: align,
+  paddingRight: `${paddingRight}px`,
+})) as typeof Typography;
+
+const FormStyled = styled('form')({
+  display: 'flex',
+  width: '100%',
+});
+
+export default Header;

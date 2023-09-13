011eb903745ba4f4d7b243a0ba34b1ba2b91c8f2
eeseung <seunghee9941@gmail.com>
2023-09-13 11:33:06 +0900

    feat: message 컴포넌트 구현

---
+++ src/components/dm/message.tsx
@@ -0,0 +1,63 @@
+import styled from '@emotion/styled';
+import Box from '@mui/material/Box';
+
+import { theme } from '@/styles/theme';
+
+interface Props {
+  message: string;
+  isSender: boolean;
+}
+
+const Message = ({ message, isSender }: Props) => {
+  return (
+    <MessageLayout sender={isSender}>
+      {isSender ? <RightBox>{message}</RightBox> : <LeftBox>{message}</LeftBox>}
+    </MessageLayout>
+  );
+};
+
+const MessageLayout = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'sender',
+})(({ sender }: { sender: boolean }) => ({
+  textAlign: `${sender === true ? 'right' : 'left'}`,
+}));
+
+const BoxStyled = styled(Box)({
+  marginBottom: '20px',
+  padding: '8px 12px',
+  position: 'relative',
+  display: 'inline-block',
+  borderRadius: '4px',
+  '&:after': {
+    content: '""',
+    position: 'absolute',
+    bottom: 0,
+    width: 0,
+    height: 0,
+    border: '20px solid transparent',
+    borderBottom: 0,
+    marginBottom: '-8px',
+  },
+});
+
+const RightBox = styled(BoxStyled)({
+  backgroundColor: `${theme.palette.primary.main}`,
+  color: 'white',
+  '&:after': {
+    right: 0,
+    borderRight: 0,
+    borderTopColor: `${theme.palette.primary.main}`,
+  },
+});
+
+const LeftBox = styled(BoxStyled)({
+  backgroundColor: '#F7F7F7',
+  color: 'black',
+  '&:after': {
+    left: 0,
+    borderLeft: 0,
+    borderTopColor: '#F7F7F7',
+  },
+});
+
+export default Message;

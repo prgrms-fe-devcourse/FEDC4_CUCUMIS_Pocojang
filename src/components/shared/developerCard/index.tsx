3aaa82f0c8284d7c7889b9f43a20c1191ccb0148
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-09-13 14:48:58 +0900

    Feature/62 developer list page (#82)
    * feat: 개발자 리스트 페이지 구현
    
    * feat: useDevelopers 훅 추가 및 적용
    
    * feat: key추가
    
    * refactor: stack 처리 변경

--- src/components/shared/developerCard/index.tsx
+++ src/components/shared/developerCard/index.tsx
@@ -1,4 +1,4 @@
-import { CardContent, Grid, Typography } from '@mui/material';
+import { CardContent, Grid, Typography, styled } from '@mui/material';
 
 import type BasicAvatarProps from '@/types/components/BasicAvatarProps';
 import BasicAvatar from '@/components/shared/avatar';
@@ -12,6 +12,7 @@ interface Props {
   name: string;
   description: string;
   to: string;
+  stacks: string[];
 }
 
 //TODO 스택은 반복문으로 변경하기,
@@ -20,6 +21,7 @@ const DeveloperCardItem = ({
   oneliner,
   name,
   description,
+  stacks,
   to,
 }: Props) => {
   return (
@@ -37,9 +39,9 @@ const DeveloperCardItem = ({
               {name}
             </Typography>
             <ChipGroup>
-              <BasicChip color="primary" label="test1"></BasicChip>
-              <BasicChip color="primary" label="test2"></BasicChip>
-              <BasicChip color="primary" label="test3"></BasicChip>
+              {stacks.slice(0, 3).map((stack) => (
+                <ChipStyled key={stack} label={stack} />
+              ))}
             </ChipGroup>
           </Grid>
         </Grid>
@@ -51,5 +53,7 @@ const DeveloperCardItem = ({
     </BasicCard>
   );
 };
-
+const ChipStyled = styled(BasicChip)({
+  maxWidth: '70px',
+});
 export default DeveloperCardItem;

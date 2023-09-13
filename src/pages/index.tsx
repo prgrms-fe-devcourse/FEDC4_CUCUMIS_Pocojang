0d9ae2eb89204cacc623c44d14f6950b202910df
nocheol <qkdl60@gmail.com>
2023-09-13 14:32:29 +0900

    feat : home page 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,69 @@
+import { Stack, Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
 
-export default function HomePage() {
+import useHome from '@/components/home/useHome';
+import ProjectCardItem from '@/components/shared/projectCard';
+import AvatarWithChip from '@/components/shared/avatarWithChip';
+
+const HomePage = () => {
+  const { projects, onlineDevelopers } = useHome();
+
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/projects/1">1번 프로젝트</Link>
-        </li>
-        <li>
-          <Link to="/projects/2">2번 프로젝트</Link>
-        </li>
-        <li>
-          <Link to="/developers/1">1번 개발자</Link>
-        </li>
-        <li>
-          <Link to="/developers/2">2번 개발자</Link>
-        </li>
-        <li>
-          <Link to="/developers/2">2번 개발자</Link>
-        </li>
-      </ul>
-    </div>
+    <Stack spacing={2}>
+      {projects.map(({ _id, projectTitle, imageUrl, name }, index) => {
+        if ((index + 1) % 2 !== 0) {
+          return (
+            <ProjectCardItem
+              key={_id}
+              name={name}
+              imageUrl={imageUrl}
+              projectTitle={projectTitle}
+              to={_id}
+            />
+          );
+        } else {
+          return (
+            <>
+              <ProjectCardItem
+                key={_id}
+                name={name}
+                imageUrl={imageUrl}
+                projectTitle={projectTitle}
+                to={_id}
+              />
+              <Box key={_id + '@'}>
+                <Typography variant="body2" gutterBottom>
+                  Developer Recommendation
+                </Typography>
+                <Stack
+                  direction="row"
+                  spacing={2}
+                  justifyContent="space-evenly"
+                >
+                  {onlineDevelopers
+                    .slice((index + 1) / 2 - 1, (index + 1) / 2 + 3)
+                    .map(({ _id: avatarId, avatarProps, label }) => (
+                      <LinkStyled to={`/dm/${avatarId}`}>
+                        <AvatarWithChip
+                          key={avatarId + '@@'}
+                          AvatarProps={avatarProps}
+                          label={label}
+                        />
+                      </LinkStyled>
+                    ))}
+                </Stack>
+              </Box>
+            </>
+          );
+        }
+      })}
+    </Stack>
   );
-}
+};
+
+const LinkStyled = styled(Link)({
+  textDecoration: 'none',
+});
+
+export default HomePage;

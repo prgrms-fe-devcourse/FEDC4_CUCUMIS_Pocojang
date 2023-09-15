8e6d8b7545424b7aafeb750aa1cc41aaad431ff4
1g2g <wonji126326@gmail.com>
2023-09-15 10:32:43 +0900

    feat: postapi 호출 로직

--- src/components/projects/useProjectDetail.ts
+++ src/components/projects/useProjectDetail.ts
@@ -1,6 +1,8 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import DUMMY_DATA from '@/consts/projectDetail';
+import { getPostId } from '@/api/posts/postId';
 
 const useProjectDetail = () => {
   const { projectId } = useParams();
@@ -10,6 +12,22 @@ const useProjectDetail = () => {
   const handleClick = (url: string, id: string) => {
     navigate(url + id);
   };
+
+  const handleProjectDetail = async (postId: string) => {
+    try {
+      await getPostId(postId);
+      // models PostType
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (projectId) {
+      handleProjectDetail(projectId);
+    }
+    // 예외처리 잘못된 요청
+  }, [projectId]);
 
   return {
     projectId,

7afc8a33a2addcf3ef62d81f5986728917beed05
kutta97 <yhjin04170@gmail.com>
2023-09-16 17:48:39 +0900

    feat: uploadPhoto api 함수에 isFormData 인자 true 로 설정

--- src/api/users/uploadPhoto/index.ts
+++ src/api/users/uploadPhoto/index.ts
@@ -2,5 +2,5 @@ import api from '@/utils/api';
 import type { UserType, FormType } from '@/types';
 
 export const uploadPhoto = async (rq: FormType): Promise<UserType> => {
-  return await api.post<FormType, UserType>('/users/upload-photo', rq);
+  return await api.post<FormType, UserType>('/users/upload-photo', rq, true);
 };

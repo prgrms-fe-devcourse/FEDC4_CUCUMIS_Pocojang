208db005818d7e7b9328feb2473c11d4ce1ad240
kutta97 <yhjin04170@gmail.com>
2023-09-18 16:44:23 +0900

    feat: serverless function 수정 - method, url 을 Custom header 에서 가져와 사용 - formData parsing 기능 추가

--- api/cucumis.ts
+++ api/cucumis.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import multipart from 'parse-multipart-data';
 
 const { API_END_POINT } = process.env;
 
@@ -7,20 +8,54 @@ const axiosInstance = axios.create({
   baseURL: API_END_POINT,
 });
 
+const getParsedMultipart = (request: VercelRequest): FormData => {
+  const boundary = request.headers['content-type']?.split('boundary=')[1] ?? '';
+  const parts = multipart.parse(request.read(), boundary);
+
+  const formData = new FormData();
+  for (let i = 0; i < parts.length; i++) {
+    const { name, data } = parts[i];
+    const value =
+      name === 'image'
+        ? (new Blob([Buffer.from(data)]) as File)
+        : data.toString('utf-8');
+
+    formData.append(name ?? '', value);
+  }
+
+  console.log('formData', formData);
+
+  return formData;
+};
+
+const getAxiosRequestConfig = (request: VercelRequest): AxiosRequestConfig => {
+  const { headers } = request;
+
+  const method = headers['api-method'] as string;
+  const url = headers['api-url'] as string;
+  const auth = headers.authorization;
+
+  const body = headers['content-type']?.includes('multipart/form-data')
+    ? getParsedMultipart(request)
+    : request.body;
+  const query = request.query;
+
+  return {
+    method,
+    url,
+    headers: { Authorization: auth },
+    data: body,
+    params: query,
+  };
+};
+
 export default async function (
   request: VercelRequest,
   response: VercelResponse,
 ) {
-  const { headers } = request;
-  const auth = headers.authorization;
-  const contentType = headers['content-type'];
+  const requestConfig = getAxiosRequestConfig(request);
 
-  const axiosConfig: AxiosRequestConfig = request.body;
-
-  const { status, data } = await axiosInstance({
-    ...axiosConfig,
-    headers: { Authorization: auth, 'Content-Type': contentType },
-  })
+  const { status, data } = await axiosInstance(requestConfig)
     .then((response: AxiosResponse) => {
       return {
         status: response.status,

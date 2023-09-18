af2f31980c897c03da028609af736a440cdf7f30
nocheol <qkdl60@gmail.com>
2023-09-18 18:02:43 +0900

    feat: 기능 개발

--- src/components/home/useHome.ts
+++ src/components/home/useHome.ts
@@ -1,133 +1,96 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import BasicAvatarProps from '@/types/components/BasicAvatarProps';
+import { getChannelPosts } from '@/api/posts/channel/channelId';
+import { PostType } from '@/types';
+
+const PROJECT_CHANNEL_ID = '6503eaffa14c752383b6a8b8';
+const DEVELOPER_CHANNEL_ID = '650557d36a9d603a4d150e7d';
+
+interface DeveloperType {
+  _id: string;
+  label: string;
+  AvatarProps: BasicAvatarProps;
+}
+
+interface HomeType {
+  _id: string;
+  imageUrl?: string;
+  name: string;
+  projectTitle: string;
+  developers?: DeveloperType[];
+}
+// TODO 동시 호출api 로 빼주기  온라인은 개발자리스트로 바꾸고 , 순수함수로 변환,홈페이지 리스를 여기서 파싱해서 만들어줘야된다.타입은 import 해오기 ,최대길이 예외처리
 const useHome = () => {
-  const projects = [
-    {
-      _id: '1',
-      projectTitle:
-        '동해물과백두산이 마르고 닳도록 하느님이보우하사우리나라만세무궁화삼천리 화려강산 대한사람대한으로 길이보전하세',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-    {
-      _id: '2',
-      projectTitle: '어쩌구저쩌구',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-    {
-      _id: '3',
-      projectTitle: '어쩌구저쩌구',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-    {
-      _id: '4',
-      projectTitle: '어쩌구저쩌구',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-    {
-      _id: '5',
-      projectTitle: '어쩌구저쩌구',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-    {
-      _id: '6',
-      projectTitle: '어쩌구저쩌구',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-    {
-      _id: '7',
-      projectTitle: '어쩌구저쩌구',
-      imageUrl: 'https://source.unsplash.com/random',
-      name: '저쩌구씨',
-    },
-  ];
+  const [homeList, setHomeList] = useState<HomeType[]>([]);
+  useEffect(() => {
+    axios
+      .all([
+        getChannelPosts({ offset: 0, limit: 3 }, PROJECT_CHANNEL_ID),
+        getChannelPosts({ offset: 0, limit: 4 }, DEVELOPER_CHANNEL_ID),
+      ])
+      .then(
+        axios.spread((projectList, developerList) => {
+          return parseHomeList(projectList, developerList);
+        }),
+      )
+      .then((list) => setHomeList(list));
+  }, []);
 
-  const onlineDevelopers = [
-    {
-      _id: '1',
+  //각각 호출해서
 
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '2',
-
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '3',
-
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '4',
-
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '5',
-
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '6',
-
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '7',
-
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-      label: '어쩌구씨',
-    },
-    {
-      _id: '8',
-      avatarProps: {
-        imgSrc: 'https://source.unsplash.com/random',
-        alt: '어쩌구씨',
-        isUserOn: true,
-      },
-
-      label: '어쩌구씨',
-    },
-  ];
-
-  return { projects, onlineDevelopers };
+  return { homeList };
 };
 export default useHome;
+
+const parseProjectPosts = (list: PostType[]) => {
+  return list.map((projectPost) => {
+    const { _id, title, author, image } = projectPost;
+    const { title: projectTitle } = JSON.parse(title);
+
+    return { _id, projectTitle, name: author.fullName, imageUrl: image };
+  });
+};
+const parseDeveloperPosts = (list: PostType[]) => {
+  return list.map((developerPost) => {
+    const {
+      _id,
+      author: { fullName, image, isOnline },
+    } = developerPost; // 여기는 개발자채널의 post id이다. 아바타 클릭시 어디로 이동?
+
+    return {
+      _id,
+      label: fullName,
+      AvatarProps: { imgSrc: image, alt: fullName, isUserOn: isOnline },
+    };
+  });
+};
+
+const parseHomeList = (projectList: PostType[], developerList: PostType[]) => {
+  const projects: HomeType[] = parseProjectPosts(projectList);
+  const developers: DeveloperType[] = parseDeveloperPosts(developerList);
+
+  projects[projects.length - 1] = {
+    ...projects[projects.length - 1],
+    developers,
+  };
+  return projects;
+};
+
+/**
+ * interface DeveloperType {
+  _id: string;
+  name: string;
+  AvatarProps: BasicAvatarProps;
+}
+
+interface HomeType {
+  _id: string;
+  imageUrl?: string;
+  name: string;
+  projectTitle: string;
+  de
+  developers?: DeveloperType[];
+}
+ */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,15 @@ import useHome from '@/components/home/useHome';
 import ProjectCardItem from '@/components/shared/projectCard';
 import AvatarWithChip from '@/components/shared/avatarWithChip';
 
+//TODO 추천 개발자 컴포넌트로 빼기
 const HomePage = () => {
-  const { projects, onlineDevelopers } = useHome();
+  const { homeList } = useHome();
 
   return (
     <Stack spacing={2} mt={1}>
-      {projects.map(({ _id, projectTitle, imageUrl, name }, index) => {
-        if ((index + 1) % 2 !== 0) {
-          return (
+      {homeList.map(({ _id, projectTitle, imageUrl, name, developers }) => {
+        return (
+          <>
             <ProjectCardItem
               key={_id}
               name={name}
@@ -21,18 +22,9 @@ const HomePage = () => {
               projectTitle={projectTitle}
               to={_id}
             />
-          );
-        } else {
-          return (
-            <>
-              <ProjectCardItem
-                key={_id}
-                name={name}
-                imageUrl={imageUrl}
-                projectTitle={projectTitle}
-                to={_id}
-              />
-              <Box key={_id + '@'}>
+
+            {developers && (
+              <Box>
                 <Typography variant="body2" gutterBottom>
                   Developer Recommendation
                 </Typography>
@@ -41,22 +33,20 @@ const HomePage = () => {
                   spacing={2}
                   justifyContent="space-evenly"
                 >
-                  {onlineDevelopers
-                    .slice((index + 1) / 2 - 1, (index + 1) / 2 + 3)
-                    .map(({ _id: avatarId, avatarProps, label }) => (
-                      <LinkStyled to={`/dm/${avatarId}`}>
-                        <AvatarWithChip
-                          key={avatarId + '@@'}
-                          AvatarProps={avatarProps}
-                          label={label}
-                        />
-                      </LinkStyled>
-                    ))}
+                  {developers.map(({ _id: avatarId, AvatarProps, label }) => (
+                    <LinkStyled to={`/developers/${avatarId}`}>
+                      <AvatarWithChip
+                        key={avatarId}
+                        AvatarProps={AvatarProps}
+                        label={label}
+                      />
+                    </LinkStyled>
+                  ))}
                 </Stack>
               </Box>
-            </>
-          );
-        }
+            )}
+          </>
+        );
       })}
     </Stack>
   );

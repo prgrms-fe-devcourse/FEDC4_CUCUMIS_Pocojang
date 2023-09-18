c8ea8dfa061c252d50a757481423f262ec31189d
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-18 16:28:30 +0900

    refactor : ProfileNav컴포넌트 interface이름 변경

--- src/components/profile/profileNav/index.tsx
+++ src/components/profile/profileNav/index.tsx
@@ -1,13 +1,13 @@
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 
-interface ProfileNavType {
+interface Props {
   value: string | number;
   navigationData: { label: number; title: string }[];
   onChange:
     | ((event: React.SyntheticEvent<Element, Event>, value: string) => void)
     | undefined;
 }
-const ProfileNav = ({ value, navigationData, onChange }: ProfileNavType) => {
+const ProfileNav = ({ value, navigationData, onChange }: Props) => {
   return (
     <BottomNavigation value={value} onChange={onChange} showLabels>
       {navigationData.map(({ label, title }) => (

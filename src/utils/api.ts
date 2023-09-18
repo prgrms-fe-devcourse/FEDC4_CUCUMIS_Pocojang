7a61499a2940bb964b010a82116f74ff43d12fe2
kutta97 <yhjin04170@gmail.com>
2023-09-18 16:42:01 +0900

    feat: axios api 모듈에서 http method 와 url 을 Custom Header 로 전달하도록 수정

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,12 +4,12 @@ import { BASE_URL } from '@/consts/api';
 import SESSION_STORAGE from '@/consts/sessionStorage';
 import session from '@/utils/sessionStorage';
 
-type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
-type RequestOptions = {
-  headers?: AxiosHeaders;
-  data?: unknown;
-  params?: unknown;
-};
+const METHOD = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+} as const;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -25,47 +25,47 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-const request = async <Response>(
-  url: string,
-  method: Method,
-  opt?: RequestOptions,
+const getCustomHeaders = (url: string, method: string): AxiosHeaders => {
+  const headers = new AxiosHeaders();
+  headers.set('api-method', method);
+  headers.set('api-url', url);
+
+  return headers;
+};
+
+const request = async <Request, Response>(
+  config: AxiosRequestConfig<Request>,
 ): Promise<Response> =>
   axiosInstance({
     method: 'POST',
-    headers: opt?.headers,
-    data: {
-      url,
-      method,
-      params: opt?.params,
-      data: opt?.data,
-    } as AxiosRequestConfig,
+    ...config,
   }).then((result: AxiosResponse) => {
     return result.data;
   });
 
-const get = async <Request, Response>(url: string, params?: Request) =>
-  request<Response>(url, 'GET', { params });
+const get = async <Request, Response>(url: string, params?: Request) => {
+  const headers = getCustomHeaders(url, METHOD.GET);
 
-const post = async <Request, Response>(
-  url: string,
-  data?: Request,
-  isFormData = false,
-) => {
-  if (isFormData) {
-    const headers = new AxiosHeaders();
-    headers.set('Content-Type', 'multipart/form-data');
-
-    return request<Response>(url, 'POST', { headers, data });
-  }
-
-  return request<Response>(url, 'POST', { data });
+  return request<Request, Response>({ headers, params });
 };
 
-const put = async <Request, Response>(url: string, data?: Request) =>
-  request<Response>(url, 'PUT', { data });
+const post = async <Request, Response>(url: string, data?: Request) => {
+  const headers = getCustomHeaders(url, METHOD.POST);
 
-const del = async <Request, Response>(url: string, data?: Request) =>
-  request<Response>(url, 'DELETE', { data });
+  return request<Request, Response>({ headers, data });
+};
+
+const put = async <Request, Response>(url: string, data?: Request) => {
+  const headers = getCustomHeaders(url, METHOD.PUT);
+
+  return request<Request, Response>({ headers, data });
+};
+
+const del = async <Request, Response>(url: string, data?: Request) => {
+  const headers = getCustomHeaders(url, METHOD.DELETE);
+
+  return request<Request, Response>({ headers, data });
+};
 
 const api = {
   get,

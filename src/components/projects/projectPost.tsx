0ee266761cb82dff6e321624373a64826e1ec8fe
1g2g <wonji126326@gmail.com>
2023-09-19 11:24:47 +0900

    feat: project 글쓰기, 글수정 구현

--- src/components/projects/projectPost.tsx
+++ src/components/projects/projectPost.tsx
@@ -1,27 +1,110 @@
+import { useNavigate } from 'react-router-dom';
 import { Button, Box, Stack } from '@mui/material';
 import styled from '@emotion/styled';
 
 import usePost from '@/components/projects/usePost';
 import BasicInput from '@/components/shared/input';
 import BasicButton from '@/components/shared/button';
+import useForm, { FormErrors, FormValues } from '@/hooks/useForm';
+import { getPostId as createPost } from '@/api/posts/create';
+import { getPostId as updatePost } from '@/api/posts/update';
+import { PROJECT_CHANNEL_ID } from '@/consts/channelId';
 
 export default function ProjectPost() {
-  const { title } = usePost();
+  const navigate = useNavigate();
 
-  return (
-    <FormBox>
+  const {
+    projectId,
+    title,
+    requirements,
+    isLoading,
+    handleFileChange,
+    selectedFile,
+    imageFile,
+  } = usePost();
+
+  const { errors, handleChange, handleSubmit } = useForm({
+    initialValues: {
+      title,
+      requirements,
+    },
+    onSubmit: async ({ title, requirements }: FormValues) => {
+      const formatedTitle = JSON.stringify({
+        title,
+        requirements,
+      });
+
+      const formData = new FormData();
+      formData.append('title', formatedTitle);
+      formData.append('channelId', PROJECT_CHANNEL_ID);
+
+      selectedFile && formData.append('image', selectedFile);
+
+      try {
+        if (projectId) {
+          formData.append('postId', projectId);
+
+          await updatePost(formData);
+
+          navigate('/projects/' + projectId);
+        } else {
+          const res = await createPost(formData);
+
+          if (res !== null) {
+            const { _id } = res;
+            navigate('/projects/' + _id);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    validate: ({ title, requirements }: FormValues) => {
+      const newErrors: FormErrors = {};
+
+      if (!title) newErrors.title = '제목을 입력해주세요.';
+      if (!requirements) newErrors.requirements = '요구사항을 입력해주세요.';
+
+      return newErrors;
+    },
+  });
+
+  return isLoading ? (
+    <Box>로딩중</Box>
+  ) : (
+    <FormBox onSubmit={handleSubmit}>
       <Stack spacing={3}>
         <Box />
-        {title ? (
-          <PreviewImageBox src={''} alt="프로필 이미지" />
+        {imageFile ? (
+          <PreviewImageBox src={imageFile} alt="프로필 이미지" />
         ) : (
           <UploadStyledButton variant="outlined" component="label">
             <Box>Click to Upload</Box>
-            <input hidden accept="image/*" multiple type="file" />
+            <input
+              hidden
+              accept="image/*"
+              multiple
+              type="file"
+              onChange={handleFileChange}
+            />
           </UploadStyledButton>
         )}
-        <BasicInput defaultValue={title} label="title" type="multiline" />
-        <BasicInput label="requirements" type="multiline" />
+        <BasicInput
+          defaultValue={title}
+          label="title"
+          type="multiline"
+          onChange={handleChange}
+          placeholder={title ? '' : '제목을 입력해주세요'}
+          errorMessage={errors.title}
+        />
+        <BasicInput
+          defaultValue={requirements}
+          label="requirements"
+          type="multiline"
+          onChange={handleChange}
+          placeholder={requirements ? '' : '요구사항을 입력해주세요'}
+          errorMessage={errors.requirements}
+        />
       </Stack>
       <BasicButtonStyled type="submit">제출하기</BasicButtonStyled>
     </FormBox>

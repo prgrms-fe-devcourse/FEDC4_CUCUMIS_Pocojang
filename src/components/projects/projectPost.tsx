25f8c482711958e03db765cbe3997256366d4965
1g2g <wonji126326@gmail.com>
2023-09-19 10:45:46 +0900

    feat: 이미지 미리보기

--- src/components/projects/projectPost.tsx
+++ src/components/projects/projectPost.tsx
@@ -3,20 +3,28 @@ import styled from '@emotion/styled';
 
 import usePost from '@/components/projects/usePost';
 import BasicInput from '@/components/shared/input';
+import BasicButton from '@/components/shared/button';
 
 export default function ProjectPost() {
-  const { title, contents } = usePost();
+  const { title } = usePost();
 
   return (
-    <Stack spacing={3}>
-      <Box />
-      <UploadStyledButton variant="outlined" component="label">
-        <Box>Click to Upload</Box>
-        <input hidden accept="image/*" multiple type="file" />
-      </UploadStyledButton>
-      <BasicInput defaultValue={title} label="title" />
-      <BasicInput defaultValue={contents} label="contents" type="multiline" />
-    </Stack>
+    <FormBox>
+      <Stack spacing={3}>
+        <Box />
+        {title ? (
+          <PreviewImageBox src={''} alt="프로필 이미지" />
+        ) : (
+          <UploadStyledButton variant="outlined" component="label">
+            <Box>Click to Upload</Box>
+            <input hidden accept="image/*" multiple type="file" />
+          </UploadStyledButton>
+        )}
+        <BasicInput defaultValue={title} label="title" type="multiline" />
+        <BasicInput label="requirements" type="multiline" />
+      </Stack>
+      <BasicButtonStyled type="submit">제출하기</BasicButtonStyled>
+    </FormBox>
   );
 }
 
@@ -25,3 +33,22 @@ const UploadStyledButton = styled(Button)({
   width: '100%',
   height: '20vh',
 }) as typeof Button;
+
+const BasicButtonStyled = styled(BasicButton)({
+  height: '56px',
+  boxSizing: 'border-box',
+});
+
+const FormBox = styled('form')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexDirection: 'column',
+  gap: '10vh',
+});
+
+const PreviewImageBox = styled('img')({
+  border: '1px dotted #c8c7c7',
+  width: '100%',
+  height: 'auto',
+  objectFit: 'cover',
+});

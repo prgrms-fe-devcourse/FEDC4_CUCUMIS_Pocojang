28cc44533c01faa1630bf8ac0fa5ba5807245154
nocheol <qkdl60@gmail.com>
2023-09-19 16:07:29 +0900

    fix:  combineReducer제거

--- src/stores/reducers.ts
+++ src/stores/reducers.ts
@@ -1,13 +1,11 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
 import { authReducer } from '@/stores/auth';
 import { layoutReducer } from '@/stores/layout';
 import { signupReducer } from '@/stores/signup';
 import { developersReducer } from '@/stores/developers';
 
-export const reducers = combineReducers({
+export const reducers = {
   auth: authReducer,
   layout: layoutReducer,
   signup: signupReducer,
   developers: developersReducer,
-});
+};

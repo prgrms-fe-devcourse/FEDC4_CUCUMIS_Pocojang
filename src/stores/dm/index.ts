4dd8661efee003244a5f7f69ebd67d286dc483f1
eeseung <seunghee9941@gmail.com>
2023-09-19 21:07:29 +0900

    feat: dmStore conversations selector 추가

--- src/stores/dm/index.ts
+++ src/stores/dm/index.ts
@@ -4,4 +4,5 @@ import * as selector from '@/stores/dm/selector';
 export const dmReducer = dmSlice.reducer;
 export const { setConversations, setDMUserId, setMessages, addMessage } =
   dmSlice.actions;
-export const { dmUserIdSelector, messagesSelector } = selector;
+export const { conversationsSelector, dmUserIdSelector, messagesSelector } =
+  selector;
--- src/stores/dm/selector.ts
+++ src/stores/dm/selector.ts
@@ -1,6 +1,8 @@
 import { RootState } from '@/stores';
-import { MessageType } from '@/types';
+import { ConversationType, MessageType } from '@/types';
 
+export const conversationsSelector = (state: RootState): ConversationType[] =>
+  state.dm.conversations;
 export const dmUserIdSelector = (state: RootState): string => state.dm.dmUserId;
 export const messagesSelector = (state: RootState): MessageType[] =>
   state.dm.messages;

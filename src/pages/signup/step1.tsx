67cc7a5bf40aa81fb4fd8bf1a98f81f33d7d0ae6
kutta97 <yhjin04170@gmail.com>
2023-09-21 15:57:50 +0900

    fix: saveAuthData 구조분해로 가져오도록 수정

--- src/pages/signup/step1.tsx
+++ src/pages/signup/step1.tsx
@@ -7,7 +7,7 @@ import { useAuth } from '@/hooks/useAuth';
 
 export default function SignupPage() {
   const navigate = useNavigate();
-  const saveAuthData = useAuth();
+  const { saveAuthData } = useAuth();
 
   const {
     signupFormValues,

99f746feed6a3ab213804370e7738979985c0d6a
kutta97 <yhjin04170@gmail.com>
2023-09-22 15:29:20 +0900

    feat: useSettingsProfileForm hook 에 isLoading 기능 추가

--- src/components/settings/profile/useSettingsProfileForm.ts
+++ src/components/settings/profile/useSettingsProfileForm.ts
@@ -1,4 +1,17 @@
+import { useEffect, useState } from 'react';
+
 import useForm, { FormErrors, FormValues } from '@/hooks/useForm';
+import { useAppDispatch, useAppSelector } from '@/stores/hooks';
+import {
+  setProfileFormValues,
+  settingsProfileFormValuesSelector,
+} from '@/stores/settings/profile';
+import { updateUser } from '@/api/settings';
+import { createPost, getPost, updatePost } from '@/api/posts';
+import CHANNEL_ID from '@/consts/channels';
+import { userProfilePostIdSelector } from '@/stores/auth/selector';
+import { setUser, userFullNameSelector } from '@/stores/auth';
+import { PostType, UserType } from '@/types';
 
 const validateProfileForm = ({ oneLiner, details }: FormValues) => {
   const newErrors: FormErrors = {};
@@ -9,33 +22,69 @@ const validateProfileForm = ({ oneLiner, details }: FormValues) => {
   return newErrors;
 };
 
-const initialProfileFormValues = {
-  name: '',
-  oneLiner: '',
-  techStack: '',
-  position: '',
-  details: '',
-};
-
 interface SettingsProfileFormParameters {
   onSuccess: () => void;
   onFail: (error: unknown) => void;
 }
 
+const updateProfile = (
+  profilePostId: string,
+  name: string,
+  formData: FormData,
+): Promise<[PostType, UserType]> =>
+  Promise.all([
+    updatePost(formData),
+    updateUser({ fullName: name, username: profilePostId }),
+  ]);
+
+const createProfile = async (
+  name: string,
+  formData: FormData,
+): Promise<UserType> => {
+  const profilePost = await createPost(formData);
+  const userData = await updateUser({
+    fullName: name,
+    username: profilePost._id,
+  });
+  return userData;
+};
+
 const useSettingsProfileForm = ({
   onSuccess,
   onFail,
 }: SettingsProfileFormParameters) => {
-  const onSubmitProfileForm = async ({
-    name,
-    oneLiner,
-    techStack,
-    position,
-    details,
-  }: FormValues) => {
+  const dispatch = useAppDispatch();
+  const profileName = useAppSelector(userFullNameSelector);
+  const profilePostId = useAppSelector(userProfilePostIdSelector);
+  const settingsProfileFormValues = useAppSelector(
+    settingsProfileFormValuesSelector,
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [defaultValues, setDefaultValues] = useState<FormValues>({
+    name: '',
+    oneLiner: '',
+    techStack: '',
+    position: '',
+    details: '',
+  });
+
+  const onSubmitProfileForm = async () => {
+    const formData = new FormData();
+    const { name, oneLiner, techStack, position, details } =
+      settingsProfileFormValues;
+
+    const title = JSON.stringify({ oneLiner, techStack, position, details });
+    formData.append('title', title);
+    formData.append('channelId', CHANNEL_ID.DEVELOPER);
+
     try {
-      // TODO: profile form 수정하기
-      console.log(name, oneLiner, techStack, position, details);
+      if (profilePostId) {
+        formData.append('postId', title);
+        await updateProfile(profilePostId, name, formData);
+      } else {
+        const userData = await createProfile(name, formData);
+        dispatch(setUser(userData));
+      }
       onSuccess();
     } catch (error) {
       onFail(error);
@@ -43,13 +92,45 @@ const useSettingsProfileForm = ({
   };
 
   const { values, errors, handleChange, handleSubmit } = useForm({
-    initialValues: initialProfileFormValues,
+    initialValues: settingsProfileFormValues,
     onSubmit: onSubmitProfileForm,
     validate: validateProfileForm,
   });
 
+  useEffect(() => {
+    if (!profileName || !profilePostId) return;
+
+    const fetchProfilePost = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getPost(profilePostId);
+        const { title } = response;
+        const profilePost = JSON.parse(title);
+
+        const profileFormValues = {
+          name: profileName,
+          ...profilePost,
+        };
+
+        setDefaultValues(profileFormValues);
+        dispatch(setProfileFormValues(profileFormValues));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProfilePost();
+  }, [profileName, profilePostId, dispatch]);
+
+  useEffect(() => {
+    dispatch(setProfileFormValues(values));
+  }, [dispatch, values]);
+
   return {
-    profileFormValues: values,
+    isLoading,
+    profileFormDefaultValues: defaultValues,
     profileFormErrors: errors,
     handleProfileFormChange: handleChange,
     handleProfileFormSubmit: handleSubmit,
--- src/pages/settings/profile/index.tsx
+++ src/pages/settings/profile/index.tsx
@@ -1,10 +1,13 @@
+import { LinearProgress } from '@mui/material';
+
 import SettingsInputContainer from '@/components/settings/SettingsInputContainer';
 import useSettingsProfileForm from '@/components/settings/profile/useSettingsProfileForm';
 import BasicInput from '@/components/shared/input';
 
 export default function ProfileSettingPage() {
   const {
-    profileFormValues,
+    isLoading,
+    profileFormDefaultValues,
     profileFormErrors,
     handleProfileFormChange,
     handleProfileFormSubmit,
@@ -17,12 +20,14 @@ export default function ProfileSettingPage() {
       console.error(error);
     },
   });
-  return (
+  return isLoading ? (
+    <LinearProgress />
+  ) : (
     <SettingsInputContainer onSubmit={handleProfileFormSubmit}>
       <BasicInput
         label="name"
         placeholder="이름을 입력해주세요"
-        value={profileFormValues.name}
+        value={profileFormDefaultValues.name}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.name}
         isRequired={false}
@@ -30,7 +35,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="oneLiner"
         placeholder="한 줄 포부를 작성해주세요"
-        value={profileFormValues.oneLiner}
+        value={profileFormDefaultValues.oneLiner}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.oneLiner}
         isRequired={false}
@@ -38,7 +43,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="techStack"
         placeholder="기술 스택을 추가해주세요"
-        value={profileFormValues.technicalTools}
+        value={profileFormDefaultValues.technicalTools}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.technicalTools}
         isRequired={false}
@@ -46,7 +51,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="position"
         placeholder="직군을 입력해주세요"
-        value={profileFormValues.position}
+        value={profileFormDefaultValues.position}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.position}
         isRequired={false}
@@ -54,7 +59,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="details"
         placeholder="소개글을 작성해주세요"
-        value={profileFormValues.details}
+        value={profileFormDefaultValues.details}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.details}
         isRequired={false}

5fccaea155aec05aecc025adfaf1836787076044
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-09-22 11:05:40 +0900

    Hotfix/profile post data binding routing (#150)
    * refactor : useProfile rollback
    
    * feat : Profile changebutton 컴포넌트 개발
    
    * fix : MUI컴포넌트 중복선언 에러 해결
    
    * fix : post게시물 클릭시 잘못된 url이동문제 해결
    
    * fix : 데이터 바인딩 문제 해결(문자열 parse필요)
    
    * fix : 스크랩 탭도 같은 버그 해결
    
    * fix : 쓸데없는 확인용 console제거

---
+++ src/components/profile/profileChangeButton/index.tsx
@@ -0,0 +1,39 @@
+import { ChangeCircle } from '@mui/icons-material';
+import { Box, IconButton, styled } from '@mui/material';
+
+interface Type {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  id: string;
+}
+
+const ProfileChangeButton = ({ onChange, id }: Type) => {
+  return (
+    <InputBoxStyled>
+      <input
+        accept="image/*"
+        id={id}
+        type="file"
+        style={{ display: 'none' }}
+        onChange={onChange}
+      />
+      <label htmlFor={id}>
+        <IconButton
+          color="primary"
+          component="span"
+          style={{ whiteSpace: 'nowrap' }}
+        >
+          <ChangeCircle />
+        </IconButton>
+      </label>
+    </InputBoxStyled>
+  );
+};
+
+const InputBoxStyled = styled(Box)({
+  position: 'absolute',
+  left: '0',
+  top: '0',
+  zIndex: '1000',
+});
+
+export default ProfileChangeButton;

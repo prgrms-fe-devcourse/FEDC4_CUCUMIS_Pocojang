899bdcff7a6f0bbbb5bae0ca627b31436ed72098
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-09-22 16:51:12 +0900

    Feature/142 search developers (#155)
    * feat: 검색기능 추가
    
    * feat: 중복 제거
    
    * feat: 로딩

--- src/stores/developers/selector.ts
+++ src/stores/developers/selector.ts
@@ -3,6 +3,6 @@ import { DeveloperType, OnlineUserType } from '@/stores/developers/slice';
 import { RootState } from '..';
 
 export const developerListSelector = (state: RootState): DeveloperType[] =>
-  state.developers.DeveloperList;
+  state.developers?.DeveloperList;
 export const onlineUserListSelector = (state: RootState): OnlineUserType[] =>
-  state.developers.onlineUserList;
+  state.developers?.onlineUserList;

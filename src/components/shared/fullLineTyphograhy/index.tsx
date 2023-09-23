19196d0712d4e01e17e2b5c5cae6792702eb51cc
1g2g <wonji126326@gmail.com>
2023-09-23 23:52:48 +0900

    fix: 띄어쓰기 없는 텍스트 줄 안 넘어가는 버그

---
+++ src/components/shared/fullLineTyphograhy/index.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { Typography, TypographyProps } from '@mui/material';
+
+interface FullLineTyphographyProps extends TypographyProps {}
+
+const FullLineTyphography = ({
+  children,
+  ...props
+}: FullLineTyphographyProps) => {
+  return <WordWrapTypography {...props}>{children}</WordWrapTypography>;
+};
+
+const WordWrapTypography = styled(Typography)({
+  wordWrap: 'break-word',
+});
+
+export default FullLineTyphography;
--- src/components/shared/itemWithAvatar/index.tsx
+++ src/components/shared/itemWithAvatar/index.tsx
@@ -14,6 +14,7 @@ import styled from '@emotion/styled';
 import BasicChip from '@/components/shared/chip';
 import BasicAvatar from '@/components/shared/avatar';
 import ItemWithAvatarProps from '@/types/components/ItemWithAvatarProps';
+import FullLineTyphography from '@/components/shared/fullLineTyphograhy';
 
 const ItemWithAvatar = ({
   children,
@@ -44,11 +45,16 @@ const ItemWithAvatar = ({
     <ListItemText
       primary={<Typography color="text.primary">{name}</Typography>}
       secondary={
-        message && (
+        message &&
+        (isComment ? (
+          <FullLineTyphography variant="body2" color="text.primary">
+            {message}
+          </FullLineTyphography>
+        ) : (
           <Typography variant="body2" color="text.primary" noWrap={!isComment}>
             {message}
           </Typography>
-        )
+        ))
       }
     />
   );
@@ -74,6 +80,8 @@ const ItemWithAvatar = ({
 
 const BoxStyled = styled(Container)({
   padding: '8px 16px',
+  width: '100%',
+  overflow: 'hidden',
 });
 
 const SkeletonStyled = styled(Skeleton)({

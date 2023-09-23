513835c7867020bb24245e3533444214c970d578
kutta97 <yhjin04170@gmail.com>
2023-09-23 18:46:15 +0900

    fix: BasicInput 초기값 을 values 대신 defaultValue 로 넣어주도록 변경

--- src/pages/settings/profile/index.tsx
+++ src/pages/settings/profile/index.tsx
@@ -27,7 +27,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="name"
         placeholder="이름을 입력해주세요"
-        value={profileFormDefaultValues.name}
+        defaultValue={profileFormDefaultValues.name}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.name}
         isRequired={false}
@@ -35,7 +35,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="oneLiner"
         placeholder="한 줄 포부를 작성해주세요"
-        value={profileFormDefaultValues.oneLiner}
+        defaultValue={profileFormDefaultValues.oneLiner}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.oneLiner}
         isRequired={false}
@@ -43,7 +43,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="techStack"
         placeholder="기술 스택을 추가해주세요"
-        value={profileFormDefaultValues.techStack}
+        defaultValue={profileFormDefaultValues.techStack}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.techStack}
         isRequired={false}
@@ -51,7 +51,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="position"
         placeholder="직군을 입력해주세요"
-        value={profileFormDefaultValues.position}
+        defaultValue={profileFormDefaultValues.position}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.position}
         isRequired={false}
@@ -59,7 +59,7 @@ export default function ProfileSettingPage() {
       <BasicInput
         label="details"
         placeholder="소개글을 작성해주세요"
-        value={profileFormDefaultValues.details}
+        defaultValue={profileFormDefaultValues.details}
         onChange={handleProfileFormChange}
         errorMessage={profileFormErrors.details}
         isRequired={false}

64145210846a6af7d6054f207966c721aee9768f
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-09-23 18:44:56 +0900

    fix : navigationProfileContent컴포넌트 랜더링 버그 해결

--- src/components/profile/navigationProfileContent/index.tsx
+++ src/components/profile/navigationProfileContent/index.tsx
@@ -9,30 +9,34 @@ interface Type {
   value: string;
 }
 
-const NavigationProfileContent  = ({ userState, value } : Type) => {
+const NavigationProfileContent = ({ userState, value }: Type) => {
   return (
     <Box>
       {userState && (
         <Box>
-          {['0', '1'].includes(value) && userState[value === '0' ? 'following' : 'followers'].map(({ user, follower }) => (
-            <ItemWithAvatar
-              key={user || follower}
-              name={user || follower}
-              AvatarProps={{ imgSrc: user || follower }}
-              to={`/profile/${user || follower}`}
-            />
-          ))}
+          {['0', '1'].includes(value) &&
+            userState[value === '0' ? 'following' : 'followers'].map(
+              ({ user, follower }) => (
+                <ItemWithAvatar
+                  key={user || follower}
+                  name={user || follower}
+                  AvatarProps={{ imgSrc: user || follower }}
+                  to={`/profile/${user || follower}`}
+                />
+              ),
+            )}
 
-          {['2', '3'].includes(value) && userState.posts.map(({ _id, title, image }) => (
-            <Box key={_id}>
-              <ProjectCardItem
-                name={JSON.parse(title).requirements}
-                imageUrl={image}
-                to={`${_id}`}
-                projectTitle={JSON.parse(title).title}
-              />
-            </Box>
-          ))}
+          {['2', '3'].includes(value) &&
+            userState.posts.map(({ _id, title, image }) => (
+              <Box key={_id}>
+                <ProjectCardItem
+                  name={JSON.parse(title).requirements}
+                  imageUrl={image}
+                  to={`${_id}`}
+                  projectTitle={JSON.parse(title).title}
+                />
+              </Box>
+            ))}
         </Box>
       )}
     </Box>

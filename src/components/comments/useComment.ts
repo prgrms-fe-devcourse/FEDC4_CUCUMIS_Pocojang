4cc06433705a43e7a8a865f99ad774ff469ca936
1g2g <wonji126326@gmail.com>
2023-09-23 15:36:57 +0900

    refactor: useComment

--- src/components/comments/useComment.ts
+++ src/components/comments/useComment.ts
@@ -1,22 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useAppSelector } from '@/stores/hooks';
 import { layoutSelector } from '@/stores/layout';
 import { createComment, deleteComment } from '@/api/comments';
-import session from '@/utils/sessionStorage';
-import type { UserType } from '@/types';
-import SESSION_STORAGE from '@/consts/sessionStorage';
 import { sendNotification } from '@/api/notifications';
 import { projectDetailSelector } from '@/stores/projectDetail/selector';
 import { LOGIN_URL, PROFILE_URL } from '@/consts/routes';
 import { tokenSelector } from '@/stores/auth/selector';
+import { userIdSelector } from '@/stores/auth';
 
 const useComment = () => {
   const { post } = useAppSelector(projectDetailSelector);
   const { input } = useAppSelector(layoutSelector);
   const token = useAppSelector(tokenSelector);
-  const [userId, setUserId] = useState('');
+  const userId = useAppSelector(userIdSelector);
+
   const navigate = useNavigate();
 
   const handleDeleteClick = async (id: string) => {
@@ -32,47 +31,37 @@ const useComment = () => {
     navigate(PROFILE_URL + id);
   };
 
-  useEffect(() => {
-    const user = session.getItem<UserType>(SESSION_STORAGE.USER);
+  const submitComment = useCallback(async () => {
+    if (!token) {
+      window.alert('로그인이 필요합니다');
 
-    if (user) {
-      const { _id } = user;
+      navigate(LOGIN_URL);
 
-      setUserId(_id);
+      return;
     }
-  }, []);
+
+    try {
+      const res = await createComment({
+        comment: input,
+        postId: post.postId,
+      });
+
+      await sendNotification({
+        notificationType: 'COMMENT',
+        notificationTypeId: res._id,
+        userId: post.author._id as string,
+        postId: post.postId,
+      });
+
+      navigate(0);
+    } catch (error) {
+      window.alert('댓글 달기에 실패하였습니다');
+    }
+  }, [token, input, navigate, post.author._id, post.postId]);
 
   useEffect(() => {
-    const submitComment = async () => {
-      if (!token) {
-        window.alert('로그인이 필요합니다');
-
-        navigate(LOGIN_URL);
-
-        return;
-      }
-
-      try {
-        const res = await createComment({
-          comment: input,
-          postId: post.postId,
-        });
-
-        await sendNotification({
-          notificationType: 'COMMENT',
-          notificationTypeId: res._id,
-          userId: post.author._id as string,
-          postId: post.postId,
-        });
-
-        navigate(0);
-      } catch (error) {
-        window.alert('댓글 달기에 실패하였습니다');
-      }
-    };
-
     input && submitComment();
-  }, [token, input, userId, navigate, post.author._id, post.postId]);
+  }, [input, submitComment]);
 
   return {
     comments: post.comments,

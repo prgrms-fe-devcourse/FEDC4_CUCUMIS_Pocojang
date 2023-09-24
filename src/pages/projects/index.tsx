6d04a0d6e1d34e81ff40bad1931769e3a72d89e2
nocheol <qkdl60@gmail.com>
2023-09-24 10:24:51 +0900

    feat: fixedProgress 적용

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,8 +1,8 @@
-import { Stack, Box, LinearProgress } from '@mui/material';
+import { Stack, Box } from '@mui/material';
 import { Add } from '@mui/icons-material';
-import styled from '@emotion/styled';
 import { useCallback } from 'react';
 
+import FixedProgress from '@/components/shared/fixedPorgress';
 import ProjectCardItem from '@/components/shared/projectCard';
 import MainFab from '@/components/shared/mainFab';
 import useProjectList from '@/components/ProjectList/useProjectList';
@@ -17,11 +17,7 @@ const ProjectPage = () => {
 
   return (
     <>
-      {isFetching && (
-        <BoxStyled>
-          <LinearProgressStyled />
-        </BoxStyled>
-      )}
+      {isFetching && <FixedProgress />}
       {isLogin && (
         <MainFab onClick={handleFabClick}>
           <Add />
@@ -44,22 +40,5 @@ const ProjectPage = () => {
     </>
   );
 };
-const BoxStyled = styled(Box)({
-  zIndex: '10',
-  maxWidth: '600px',
-  top: '0',
-  height: '66px',
-  width: '100%',
-  position: 'fixed',
-  left: '50%',
-  transform: 'translate(-50%)',
-  pointerEvents: 'none',
-});
-const LinearProgressStyled = styled(LinearProgress)({
-  position: 'absolute',
-  bottom: '0',
-  height: '10px',
-  width: '100%',
-});
 
 export default ProjectPage;
